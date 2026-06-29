975adbb510016be6d10f7fd0fb99751e4228f6e8
Dhruv-Kap <0603dhruv@gmail.com>
2026-06-29 13:33:21 +0530

    Restore working build

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Ascentt-AR_car_engine_repair-0.1.0";
+const contentToCache = [
+    "Build/AR_Project.loader.js",
+    "Build/AR_Project.framework.js",
+    "Build/AR_Project.data",
+    "Build/AR_Project.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
